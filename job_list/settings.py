2b8a82b32574c5115c75002a6353c1b775cd7b2f
antonychalks <antonychalkley@hotmail.co.uk>
2024-02-21 14:52:54 +0000

    Initial commit, installed Django, created planners and tradesman app

--- job_list/settings.py
+++ job_list/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^hgd#rjtheiars=zb5f!u=y9v341nx66^4esinh&3+na1=-+4#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'planners',
+    'tradesman',
 ]
 
 MIDDLEWARE = [
