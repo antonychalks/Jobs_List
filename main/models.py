a04e0cd43780ee75a48c5c0d9b8334e4c4377df2
antonychalks <antonychalkley@hotmail.co.uk>
2024-02-24 19:40:58 +0000

    Add more details to the UserProfile Model. Added these options to the user_detail view

--- main/models.py
+++ main/models.py
@@ -30,10 +30,24 @@ class UserProfile (models.Model):
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=50)
     slug = models.SlugField(max_length=200, unique=True)
+    medical = models.TextField(blank=True)
+    emergency_name = models.CharField(max_length=50, blank=True)
+    emergency_number = models.IntegerField(blank=True, default='0')
+    certifications = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+    address_ln_1 = models.CharField(blank=True)
+    address_ln_2 = models.CharField(blank=True)
+    address_ln_3 = models.CharField(blank=True)
+    postcode = models.CharField(blank=True)
+    phone = models.IntegerField(blank=True, default='0')
+    other_phone = models.IntegerField(blank=True, default='0')
     
     class Meta:
         ordering = ["role"]
     
     def __str__(self):
         return f"{self.user} | {self.role}"
+    
+    def get_trade_display(self):
+        return ", ".join([dict(TRADES)[trade] for trade in self.trade])
     
