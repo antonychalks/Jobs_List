e3bb865dcbea6a0bdd6769b90d08448005e88878
antonychalks <antonychalkley@hotmail.co.uk>
2024-02-26 18:19:55 +0000

    Created Static Filed

--- job_list/settings.py
+++ job_list/settings.py
@@ -27,9 +27,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '.herokuapp.com'
+]
 
 
 # Application definition
@@ -151,6 +154,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- tradesman/models.py
+++ tradesman/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from multiselectfield import MultiSelectField
+from cloudinary.models import CloudinaryField
+from main.models import TRADES
 
 # Create your models here.
+class Job (models.Model):
+    """
+    Stores the profile of a user
+
+    """
+    slug = models.SlugField(max_length=200, unique=True)
+    trades_required = MultiSelectField(max_length=30, choices=TRADES)
+    job_number = models.IntegerField()
+    phone = models.CharField(blank=True)
+    other_phone = models.CharField('Other Phone Number', blank=True)
+    email = models.EmailField(blank=True)
+    street = models.CharField(blank=True)
+    town_city = models.CharField('Town/City', blank=True)
+    county = models.CharField(blank=True)
+    postcode = models.CharField(blank=True)
+    job_description = models.TextField()
+    add_task = models.CharField()
+    pending_task = models.CharField()
+    
+    def get_trade_display(self):
+        return ", ".join([dict(TRADES)[trade] for trade in self.trade])
