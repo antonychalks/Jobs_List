ecbff1379b63f0ecedc089a9c5df4a76478beaef
antonychalks <antonychalkley@hotmail.co.uk>
2024-03-09 19:17:27 +0000

    Set debug to False and collectstatic ready for deployment

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,143 +1,25 @@
-console.log("script_job_detail.js loaded")
-const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
-const deleteButtons = document.getElementsByClassName("btn-delete");
-const addTaskButton = $("#add_task_button");
-const cancelButton = $("#cancel_button");
-const editButtons = document.getElementsByClassName("btn-edit");
-const submitButton = document.getElementById("submitButton");
-const editTaskForm = $("#edit_task_form")
-const TRADES = [
-    ["0", "Carpenter"],
-    ["1", "Plumber"],
-    ["2", "Electrician"],
-    ["3", "Plasterer"],
-    ["4", "Groundsworker"],
-    ["5", "Decorator"],
-    ["6", "Gas"],
-    ["7", "Planner"]
-];
-
 $(document).ready(function() {
-
-    // Hide the add_task_form by default
-    $("#add_task_form").hide();
-    $("#edit_task_form").hide();
+    setStatusColor()
 });
 
-// Function to handle when the add task button is clicked
-addTaskButton.on("click", function() {
-    console.log("Add Task button clicked");
+function setStatusColor() {
+    console.log("Setting status color");
+    $(".job_status").each(function() {
+        const status = $(this);
+        const statusText = status.text().trim(); // Use .text() to retrieve text content and trim whitespace
+        console.log("Status Text:", statusText); // Log status text for debugging
+        // Remove existing classes to ensure only one class is added
+        status.removeClass("bg-info bg-danger bg-warning bg-success");
 
-    // Show the add task form
-    $("#add_task_form").show();
-    addTaskButton.hide();
-    $("button[type='submit']").attr("name", "add_task");
-    console.log("Submit button name attribute changed to add_task");
-});
-
-    // Function to handle when the cancel button is clicked
-    cancelButton.on("click", function() {
-        console.log("Cancel button clicked");
-
-        // Hide the add task form and show the add task button
-        $("#add_task_form").hide();
-        addTaskButton.show();
+        // Determine which class to add based on the status text
+        if (statusText === "Unassigned") {
+            status.addClass("bg-info");
+        } else if (statusText === "Pending Start") {
+            status.addClass("bg-danger");
+        } else if (statusText === "In Progress") {
+            status.addClass("bg-warning");
+        } else if (statusText === "Completed") {
+            status.addClass("bg-success");
+        }
     });
-
-// Handle click event for edit button
-$(".edit-button").on("click", function() {
-    console.log("Edit Task button clicked");
-
-    var taskId = $(this).data("task_id");
-    var description = $(this).data("description");
-    var tradesRequired = $(this).data("trades-required");
-    var tradesmanAssigned = $(this).data("tradesman-assigned");
-    var timeRequired = $(this).data("time-required");
-    var isCompleted = $(this).data("is-completed");
-    var slug = $(this).data("slug");
-    console.log(taskId);
-    console.log(slug);
-    console.log("Trades Required:", tradesRequired);
-
-    // Populate the add_task_form fields with task details
-    $("#id_description").val(description);
-    // For trades_required, set the checkboxes based on the received data
-    setTradesCheckboxes(tradesRequired);
-    $("#id_tradesman_assigned").val(tradesmanAssigned);
-    $("#id_time_required").val(timeRequired);
-    // Set the is_completed checkbox based on the data
-    $("#id_is_completed").prop("checked", isCompleted === "True");
-
-    // Change the submit button name to indicate editing the task
-    $("button[type='submit']").attr("name", "edit_task");
-    $("button[type='submit']").addClass( "btn-edit" );
-    // Show the add_task_form
-    $("#edit_task_form").show();
-    addTaskButton.hide();
-    editButtons.innerText = "Update";
-    var url = `/tradesman/${slug}/edit_task/${taskId}/`;
-    console.log("Edit URL:", url);
-    editTaskForm.attr("action", `edit_task/${taskId}/`);
-});
-
-/**
-* Initializes deletion functionality for the provided delete buttons.
-* 
-* For each button in the `deleteButtons` collection:
-* - Retrieves the associated comment's ID upon click.
-* - Updates the `deleteConfirm` link's href to point to the 
-* deletion endpoint for the specific comment.
-* - Displays a confirmation modal (`deleteModal`) to prompt 
-* the user for confirmation before deletion.
-*/
-$(".btn-delete").on("click", function() {
-    let taskId = $(this).data("task_id");
-    console.log(taskId)
-    deleteConfirm.href = `delete_task/${taskId}`;
-    deleteModal.show();
-});
-
-
-    // Function to set the checkboxes for trades_required
-function setTradesCheckboxes(tradesRequired) {
-    console.log("Setting trades checkboxes. Trades Required:", tradesRequired);
-    // Reset checkboxes first
-    $("input[name^='trades_required']").prop("checked", false);
-    // Check if tradesRequired is a string
-    if (typeof tradesRequired === 'string') {
-        // Split the string into an array of trade codes
-        var tradeCodes = tradesRequired.split(", ");
-        console.log("Trade Codes:", tradeCodes);
-        // Set checkboxes for each trade code
-        tradeCodes.forEach(tradeCode => {
-            // Find the trade in the TRADES array
-            const tradeIndex = TRADES.findIndex(trade => trade[1] === tradeCode);
-            if (tradeIndex !== -1) {
-                // Construct the ID based on the index
-                var checkboxId = "#id_trades_required_" + TRADES[tradeIndex][0];
-                console.log("Setting checkbox for trade:", tradeCode, "ID:", checkboxId);
-                $(checkboxId).prop("checked", true);
-            } else {
-                console.error("Trade code not found:", tradeCode);
-            }
-        });
-    } else if (Array.isArray(tradesRequired)) {
-        // If tradesRequired is already an array, set checkboxes for each trade code
-        tradesRequired.forEach(tradeCode => {
-            // Find the trade in the TRADES array
-            const tradeIndex = TRADES.findIndex(trade => trade[1] === tradeCode);
-            if (tradeIndex !== -1) {
-                // Construct the ID based on the index
-                var checkboxId = "#id_trades_required_" + TRADES[tradeIndex][0];
-                console.log("Setting checkbox for trade:", tradeCode, "ID:", checkboxId);
-                $(checkboxId).prop("checked", true);
-            } else {
-                console.error("Trade code not found:", tradeCode);
-            }
-        });
-    } else {
-        console.error("tradesRequired is not in a valid format:", tradesRequired);
-    }
 }
-
-
---
+++ staticfiles/js/script_planner_home.js
@@ -0,0 +1,85 @@
+
+const addJobButton = $("#add_job_button");
+const cancelButton = $(".cancel_button");
+const editJobForm = $("#edit_job_form")
+
+
+
+// Function to handle when the add task button is clicked
+addJobButton.on("click", function() {
+    console.log("Add Job button clicked");
+
+    // Show the add task form
+    $("#add_job_form").show();
+    addJobButton.hide();
+    $("button[type='submit']").attr("name", "add_job");
+    console.log("Submit button name attribute changed to add_job");
+});
+
+// Function to handle when the cancel button is clicked
+cancelButton.on("click", function() {
+    console.log("Cancel button clicked");
+
+    // Hide the add task form and show the add task button
+    $("#add_job_form").hide();
+    $("#edit_job_form").hide();
+    addJobButton.show();
+});
+
+// Handle click event for edit button
+$(".edit-job-button").on("click", function() {
+    console.log("Edit Job button clicked");
+
+    var jobId = $(this).data("job_id");
+    var slug = $(this).data("slug");
+    var customerName = $(this).data("customer_name");
+    var phone = $(this).data("phone");
+    var otherPhone = $(this).data("other_phone");
+    var email = $(this).data("email");
+    var street = $(this).data("street");
+    var townCity = $(this).data("town_city");
+    var county = $(this).data("county");
+    var postcode = $(this).data("postcode");
+    var jobDescription = $(this).data("job_description");
+
+
+    // Populate the add_task_form fields with task details
+    $("#id_customer_name").val(customerName);
+    $("#id_phone").val(phone);
+    $("#id_other_phone").val(otherPhone);
+    $("#id_email").val(email);
+    $("#id_street").val(street);
+    $("#id_town_city").val(townCity);
+    $("#id_county").val(county);
+    $("#id_postcode").val(postcode);
+    $("#id_job_description").val(jobDescription);
+    
+    // Show the edit_job_form
+    $("#edit_job_form").show();
+    addJobButton.hide();
+    var url = `/planners/${slug}/edit_job/${jobId}/`;
+    console.log("Edit URL:", url);
+    editJobForm.attr("action", `/planners/${slug}/edit_job/${jobId}/`);
+});
+
+function setStatusColor() {
+    console.log("Setting status color");
+    $(".job_status").each(function() {
+        const status = $(this);
+        const statusText = status.html();
+
+        // Remove existing classes to ensure only one class is added
+        status.removeClass("bg-info bg-danger bg-warning bg-success");
+
+        // Determine which class to add based on the status text
+        if (statusText === "Unassigned") {
+            status.addClass("bg-info");
+        } else if (statusText === "Pending Start") {
+            status.addClass("bg-danger");
+        } else if (statusText === "In Progress") {
+            status.addClass("bg-warning");
+        } else if (statusText === "Complete") {
+            status.addClass("bg-success");
+        }
+    });
+}
