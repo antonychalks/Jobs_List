62161553cd2cae42e46f61635ea448d75dc300e5
antony <antonychalkley@hotmail.co.uk>
2024-09-10 17:54:18 +0100

    Created test cases for all the views in the main app

--- main/test_views.py
+++ main/test_views.py
@@ -7,35 +7,66 @@ from tradesman.models import Job, Task
 class TestMainViews(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='password')
-        self.user_profile = UserProfile.objects.create(
+        self.user = User.objects.create_user(username='testUser', password='password')
+        self.profile = UserProfile.objects.get(
             user=self.user,
-            role=0,
-            slug='testuser'
         )
-
-        self.job = Job.objects.create(
-            created_by=self.user,
-            customer_name="Test Customer",
-            status=0,
-            slug='test-job'
-        )
-
-        self.task = Task.objects.create(
-            job=self.job,
-            description="Test Task",
-            is_completed=False
-        )
+        self.profile.role = 0
+        self.profile.slug = 'testUser'
+        self.profile.fname = 'testFName'
+        self.profile.lname = 'testLName'
+        self.profile.medical = 'testMedical'
+        self.profile.nok = 'testNok'
+        self.profile.nok_number = 'testNok_number'
+        self.profile.certifications = 'testCertifications'
+        self.profile.email = 'testEmail'
+        self.profile.street = 'testStreet'
+        self.profile.town_city = 'testTownCity'
+        self.profile.county = 'testCounty'
+        self.profile.postcode = 'testPostcode'
+        self.profile.phone = 'testPhone'
+        self.profile.profile_complete = True
+        self.profile.is_initial_signup = False
+        self.profile.save()
 
     def test_landing_page_view_authenticated_user(self):
-        self.client.login(username='testuser', password='password')
-        response = self.client.get(reverse('landing_page'))
+        self.client.login(username='testUser', password='password')
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'main/index.html')
-        self.assertEqual(response.context['user_profile'], self.user_profile)
+        self.assertEqual(response.context['user_profile'], self.profile)
 
     def test_landing_page_view_unauthenticated_user(self):
-        response = self.client.get(reverse('landing_page'))
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'main/index.html')
         self.assertIsNone(response.context['user_profile'])
+
+    def test_landing_page_view_initial_signup(self):
+        self.client.login(username='testUser', password='password')
+        self.profile.is_initial_signup = True
+        self.profile.save()
+        response = self.client.get(reverse('home'))
+        self.assertRedirects(response, reverse('user_profile_signup'), status_code=302, target_status_code=200)
+
+    def test_user_profile_signup_view(self):
+        self.client.login(username='testUser', password='password')
+        self.profile.is_initial_signup = True
+        self.profile.save()
+        response = self.client.get(reverse('user_profile_signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/user_profile_signup.html')
+        self.assertEqual(response.context['user'], self.user)
+
+    def test_user_profile_signup_view_submit_form(self):
+        self.client.login(username='testUser', password='password')
+        response = self.client.post(reverse('user_profile_signup'), data={
+            'fname': 'testFName',
+            'lname': 'testLName',
+            'email': 'testEmail',
+            'phone': 'testPhone',
+            'role': 0,
+            'trade': 'pl',
+        })
+
+        self.assertEqual(response.status_code, 200)
